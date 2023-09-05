fa494816f4f1b8a43bcef5b025d8889d378cf375
Yusuf Swelam <yusufswelam2@gmail.com>
2023-09-05 03:50:20 -0700

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+upper = False
+for i in range(122, 96, -1):
+    char = ""
+    if upper:
+        char = chr(i - 32)
+    else:
+        char = chr(i)
+    upper = not(upper)
+    print("{}".format(char), end="")
