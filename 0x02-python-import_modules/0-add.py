a5d4782fbb7bd9a4caebb9d6906e6897ce57e18e
Yusuf Swelam <yusufswelam2@gmail.com>
2023-09-07 06:30:13 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+
+    print("{} + {} = {}".format(1, 2, add(1, 2)))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    
+    print("{} + {} = {}".format(10, 5, add(10, 5)))
+    print("{} - {} = {}".format(10, 5, sub(10, 5)))
+    print("{} * {} = {}".format(10, 5, mul(10, 5)))
+    print("{} / {} = {}".format(10, 5, div(10, 5)))
