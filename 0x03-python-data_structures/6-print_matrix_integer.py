f40a5d0a77e75ba25755fb5474e8255afaec1331
Yusuf Swelam <yusufswelam2@gmail.com>
2023-09-08 04:55:35 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i]) - 1):
+            print("{}".format(matrix[i][j]), end=" ")
+        if len(matrix[i]) > 0:
+            print("{}".format(matrix[i][-1]))
+        else:
+            print()
