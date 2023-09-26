57b1572e92e5edbfeb4ec34c8bc663fc04742714
Yusuf Swelam <yusufswelam2@gmail.com>
2023-09-26 14:53:23 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             n += 1
-        except:
+        except Exception as e:
             break
     print()
     return n
