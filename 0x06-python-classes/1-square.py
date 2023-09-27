0579d50351ad0f3de70440a5c0339561075bb3b4
Yusuf Swelam <yusufswelam2@gmail.com>
2023-09-27 12:08:56 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Constructor.
+
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
