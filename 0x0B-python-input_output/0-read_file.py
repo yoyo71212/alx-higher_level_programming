a59979cc94ddc02b7e37962fae82b0a541eaf771
Yusuf Swelam <yusufswelam2@gmail.com>
2023-10-11 13:07:53 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""defining read_file function"""
+
+
 def read_file(filename=""):
+    """Reads a file, filename, using utf-8 encoding"""
+
     with open(filename, encoding='utf-8') as file:
         print(file.read(), end='')
