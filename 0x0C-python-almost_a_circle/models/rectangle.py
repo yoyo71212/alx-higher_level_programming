c25e6ad998580061007afd145f9f5f292371a0aa
yoyo71212 <yusufswelam2@gmail.com>
2023-10-16 08:54:21 -0700

    15. Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,5 +102,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """ Returns the dictionary representation of a Rectangle """
-        return {"id": self.id, "height": self.height, "width": self.width,
-                "x": self.x, "y": self.y}
+        return {"x": self.x, "width": self.width, "id": self.id,
+                "height": self.height, "y": self.y}
