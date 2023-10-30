f0314e574bdfc0c0affbd17b9d144dce8eb63f8a
Yusuf Swelam <yusufswelam2@gmail.com>
2023-10-29 19:24:04 -0700

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module for Square class """
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Defines Square class """
+    def __init__(self, size):
+        """ Square constructor """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ Returns the area of a Square """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ Returns the string representation of a Square """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
