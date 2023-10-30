4d9857b2e60343bc81aed65075cf23b58101da1e
Yusuf Swelam <yusufswelam2@gmail.com>
2023-10-29 18:53:04 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """ Defines BaseGeometry class """
+    def area(self):
+        """ Returns the area """
+        raise Exception("area() is not implemented")
